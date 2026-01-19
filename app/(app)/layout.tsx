e71009f09ae2961cac6d7c522e6de00ec640d0c6
bartoszb0 <bartoszb2020@gmail.com>
2026-01-19 16:29:26 +0100

    modified layout

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -7,11 +7,9 @@ export default function ProtectedLayout({
 }) {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="flex-1 w-full flex flex-col gap-7 items-center">
         <Header />
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          {children}
-        </div>
+        <div className="flex-1 flex flex-col gap-20 max-w-5xl">{children}</div>
       </div>
     </main>
   );
