e2ad2dd2996a8a3882c51695e4465f35eb552374
bartoszb0 <bartoszb2020@gmail.com>
2026-01-19 16:30:54 +0100

    added helper function for redirecting user if he's not authenticated

---
+++ lib/getRequiredUser.ts
@@ -0,0 +1,16 @@
+import { redirect } from "next/navigation";
+import { createClient } from "./supabase/server";
+
+export async function getRequiredUser() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    redirect("/login");
+  }
+
+  return user;
+}
